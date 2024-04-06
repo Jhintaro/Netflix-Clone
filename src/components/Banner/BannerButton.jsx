c94a24018545177d793e15af94e7ca7966cb634f
Jhintaro <ahmedx2233@outlook.com>
2024-04-06 12:33:48 +0300

    bannerbutton

--- src/components/Banner/BannerButton.jsx
+++ src/components/Banner/BannerButton.jsx
@@ -7,7 +7,7 @@ const BannerButton = ({ children, onClick }) => {
 
   return (
     <button
-      className="cursor-pointer text-white outline-none border-none font-bold rounded-[0.2vw] pl-8 pr-8 mr-4 mt-2 bg-black/50 pb-2 hover:text-black hover:bg-white transition-all duration-200 pt-2"
+      className="cursor-pointer text-white outline-none border-none font-bold rounded-[0.2vw] pl-8 pr-8 mr-4 mt-2 bg-black/50 pb-2 hover:text-black hover:bg-white transition-all duration-200 pt-2 w-[120px]"
       onClick={handleClick}
     >
       {children}
